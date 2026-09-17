1a197c9a0d328d43425cfa04c3036e3f1566c3b8
dogelkowy <michalek.miki2008@gmail.com>
2026-09-17 19:04:20 +0200

    Fix project not building

--- src/app/main/dashboard/dashboard.ts
+++ src/app/main/dashboard/dashboard.ts
@@ -2,6 +2,7 @@ import { ChangeDetectorRef, Component, inject } from '@angular/core';
 import { ProductService } from '../../service/products-service/product.service';
 import { LocationsService } from '../../service/location-service/locations-service';
 import { AuthService } from '../../service/auth-service/auth-service';
+import { Product } from '../../service/products-service/product';
 
 @Component({
   selector: 'app-dashboard',
